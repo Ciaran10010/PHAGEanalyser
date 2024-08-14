70d5888c4e19a070df2ad3d230ba4cfdf196ca29
Ciaran10010 <101805439+Ciaran10010@users.noreply.github.com>
2024-08-14 15:47:00 +0100

    Further work on filesystem
    Started adding elements that allow the code to be run from anywhere and the raw reads to be used from anywhere. Also started, but not finished code that will preserve the original reads for easier error checking

--- scriptv2.sh
+++ scriptv2.sh
@@ -8,7 +8,9 @@ echo Please input the directory of the raw Illumina reads.
 read rd
 
 for phage_directory in "$rd"/*; do
-	mkdir "$phage_directory"/fastqc 
+	phage_folder=${phage_directory##*/}
+	echo $phage_folder
+	mkdir "$phage_directory"/fastqc
 	mkdir "$phage_directory"/log
 	touch "$phage_directory"/log/initial_fastqc.txt
 	echo Performing FastQC analysis of initial reads
@@ -117,76 +119,81 @@ EOF
 		fi
 	done
 	
-	#~ source /home/ciaran/anaconda3/bin/activate sam
+	 source /home/ciaran/anaconda3/bin/activate sam
 
-	#~ apc.pl "$phage_directory"/pilon_read.fasta
+	apc.pl "$phage_directory"/pilon_read.fasta
 
-	#~ mv permuted* "$phage_directory"
+	mv permuted* "$phage_directory"
 
-	#~ source /home/ciaran/anaconda3/bin/activate main
+	source /home/ciaran/anaconda3/bin/activate main
 	
-	#~ if [ -e $phage_directory/permuted.1.fa ];
-	#~ then
-		#~ phage_to_blast=$phage_directory/permuted.1.fa
-	#~ else
-		#~ phage_to_blast=$phage_directory/pilon_read.fasta
-	#~ fi
+	if [ -e $phage_directory/permuted.1.fa ];
+	then
+		phage_to_blast=$phage_directory/permuted.1.fa
+	else
+		phage_to_blast=$phage_directory/pilon_read.fasta
+	fi
 	
-	#~ echo 'BLASTing the genome (this will take some time)'
-	#~ blastn -query "$phage_to_blast" -remote -db nr -out "$phage_directory"/blast_result.txt -outfmt 6 -evalue 1e-30
+	echo 'BLASTing the genome (this will take some time)'
+	blastn -query "$phage_to_blast" -remote -db nr -out "$phage_directory"/blast_result.txt -outfmt 6 -evalue 1e-30
 
-	#~ accession_number=$(awk -v line=3 'NR==3 {print $2}' "$phage_directory"/blast_result.txt)
-	#~ echo $accession_number
-	#~ datasets download virus genome accession "$accession_number" --include cds genome
+	accession_number=$(awk -v line=3 'NR==3 {print $2}' "$phage_directory"/blast_result.txt)
+	echo $accession_number
+	datasets download virus genome accession "$accession_number" --include cds genome
 
-	#~ mv ncbi_dataset.zip "$phage_directory"
+	mv ncbi_dataset.zip "$phage_directory"
 
-	#~ unzip "$phage_directory"/ncbi_dataset.zip -d "$phage_directory"
+	unzip "$phage_directory"/ncbi_dataset.zip -d "$phage_directory"
 
-	#~ tail -n +2 "$phage_directory"/ncbi_dataset/data/cds.fna > "$phage_directory"/ncbi_dataset/data/processed_cds.fna
+	tail -n +2 "$phage_directory"/ncbi_dataset/data/cds.fna > "$phage_directory"/ncbi_dataset/data/processed_cds.fna
 
-	#~ first_gene_end=$(awk '/^>/ {print NR-1;exit}' "$phage_directory"/ncbi_dataset/data/processed_cds.fna)
+	first_gene_end=$(awk '/^>/ {print NR-1;exit}' "$phage_directory"/ncbi_dataset/data/processed_cds.fna)
 
-	#~ sed -n 1,"$first_gene_end"p "$phage_directory"/ncbi_dataset/data/processed_cds.fna > "$phage_directory"/first_gene.fasta
+	sed -n 1,"$first_gene_end"p "$phage_directory"/ncbi_dataset/data/processed_cds.fna > "$phage_directory"/first_gene.fasta
 
-	#~ start_of_phage=$(blastn -outfmt "7 sstart" -query  "$phage_directory"/first_gene.fasta -subject "$phage_directory"/permuted.1.fa | grep -v '^#')
+	start_of_phage=$(blastn -outfmt "7 sstart" -query  "$phage_directory"/first_gene.fasta -subject "$phage_directory"/permuted.1.fa | grep -v '^#')
 
-	#~ cat > "$phage_directory"/reorder.py <<EOF
+	cat > "$phage_directory"/reorder.py <<EOF
 
-#~ from Bio import SeqIO
+from Bio import SeqIO
 
-#~ for contig_record in SeqIO.parse(open('$phage_directory/permuted.1.fa'), 'fasta'):
-    #~ contig = str(contig_record.seq)
+for contig_record in SeqIO.parse(open('$phage_to_blast'), 'fasta'):
+    contig = str(contig_record.seq)
 
-#~ str1= contig[$start_of_phage:]
-#~ str2= contig[:$start_of_phage]
-#~ str_final='>'+contig_record.id + '\n' + str1 + str2
-#~ with open("$phage_directory/reordered_genome.fasta", "w") as out:
-    #~ out.write(str_final)
+str1= contig[$start_of_phage:]
+str2= contig[:$start_of_phage]
+str_final='>'+contig_record.id + '\n' + str1 + str2
+with open("$phage_directory/reordered_genome.fasta", "w") as out:
+    out.write(str_final)
 
-#~ EOF
+EOF
 
-	#~ python3 "$phage_directory"/reorder.py
+	python3 "$phage_directory"/reorder.py
 
-	#~ bbmap.sh ref="$phage_directory"/reordered_genome.fasta in1="$phage_directory"/clean_1.fastq in2="$phage_directory"/clean_2.fastq covstats="$phage_directory"/final_covstats.txt out="$phage_directory"/all_reads_contig_mapped.sam
+	bbmap.sh ref="$phage_directory"/reordered_genome.fasta in1="$phage_directory"/clean_1.fastq in2="$phage_directory"/clean_2.fastq covstats="$phage_directory"/final_covstats.txt out="$phage_directory"/all_reads_contig_mapped.sam
 
-	#~ source /home/ciaran/anaconda3/bin/activate sam
+	source /home/ciaran/anaconda3/bin/activate sam
 	
-	#~ samtools view -bS -F4 "$phage_directory"/all_reads_contig_mapped.sam | samtools sort - -o "$phage_directory"/all_reads_contig_mapped_sorted.bam
+	samtools view -bS -F4 "$phage_directory"/all_reads_contig_mapped.sam | samtools sort - -o "$phage_directory"/all_reads_contig_mapped_sorted.bam
 	
-	#~ samtools index "$phage_directory"/all_reads_contig_mapped_sorted.bam
+	samtools index "$phage_directory"/all_reads_contig_mapped_sorted.bam
 	
-	#~ samtools view -bS -f4 "$phage_directory"/all_reads_contig_mapped.sam | samtools sort - -o "$phage_directory"/non_mapped_sorted.bam
+	samtools view -bS -f4 "$phage_directory"/all_reads_contig_mapped.sam | samtools sort - -o "$phage_directory"/non_mapped_sorted.bam
 	
-	#~ samtools fastq "$phage_directory"/non_mapped_sorted.bam > "$phage_directory"/non_mapped_reads.fq
+	samtools fastq "$phage_directory"/non_mapped_sorted.bam > "$phage_directory"/non_mapped_reads.fq
 	
-	#~ source /home/ciaran/anaconda3/bin/activate main
+	source /home/ciaran/anaconda3/bin/activate main
 	
-	#~ pilon --genome "$phage_directory"/reordered_genome.fasta --frags "$phage_directory"/all_reads_contig_mapped_sorted.bam --output "$phage_directory"/final_genome --verbose --changes
+	pilon --genome "$phage_directory"/reordered_genome.fasta --frags "$phage_directory"/all_reads_contig_mapped_sorted.bam --output "$phage_directory"/final_genome --verbose --changes
 	
-	#~ sed "1s/.*/>contig/" "$phage_directory"/final_genome.fasta > "$phage_directory"/prokka_input.fasta
+	sed "1s/.*/>contig/" "$phage_directory"/final_genome.fasta > "$phage_directory"/prokka_input.fasta
 	
-	#~ prokka --outdir "$phage_directory"/prokka --prefix genome --kingdom viruses "$phage_directory"/prokka_input.fasta
+	prokka --outdir "$phage_directory"/prokka --prefix genome --kingdom viruses "$phage_directory"/prokka_input.fasta
+
+	mkdir -p $script_dir/output/$phage_folder
+	mv $phage_directory/* $script_dir/output/$phage_folder
+	mv $script_dir/output/$phage_folder/$read_1 $phage_directory
+	mv $script_dir/output/$phage_folder/$read_2 $phage_directory
 	
 	
 	
